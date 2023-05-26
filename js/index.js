df3cd731ebed77b9132c052bbd8141534fac4ca5
Teresa <teresa.juengel@posteo.de>
2023-05-26 12:40:38 +0200

    add functionality to answer-button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 const body = document.querySelector('[data-js="body"]');
 const buttonDarkmode = document.querySelector('[data-js="button-darkmode"]');
 
-buttonDarkmode.addEventListener("click", () => {
+buttonDarkmode?.addEventListener("click", () => {
   body.classList.toggle("darkmode");
+});
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-js="hidden-answer"]');
+
+answerButton?.addEventListener("click", () => {
+  answer.classList.toggle("questioncard__answer");
+
+  const buttonInitalText = "Show Answer";
+  if (answerButton.textContent.includes(buttonInitalText)) {
+    answerButton.textContent = "HIde Answer";
+  } else {
+    answerButton.textContent = buttonInitalText;
+  }
 });
