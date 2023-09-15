ed57b5d21e2ce53bce4770fa855ef741cabfe2ae
Teresa Juengel <teresa.juengel@posteo.de>
2023-09-15 15:36:05 +0200

    add function for counting bookmarks

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,12 @@
 const body = document.querySelector('[data-js="body"]');
 const buttonDarkmode = document.querySelector('[data-js="button-darkmode"]');
+const counterBookmarks = document.querySelector(
+  '[data-js="counter-bookmarks"]'
+);
 
-buttonDarkmode?.addEventListener("click", () => {
+buttonDarkmode.addEventListener("click", () => {
   body.classList.toggle("darkmode");
 });
+
+// const countBookmarks = localStorage.getItem("quizAppCounterBookmarks");
+// counterBookmarks.textContent = countBookmarks;
