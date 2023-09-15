bb48fc81c6dce3df682db1144831710be308429c
Teresa Juengel <teresa.juengel@posteo.de>
2023-09-15 13:54:39 +0200

    add darkmode

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,6 @@
-// const buttonDarkmode = document.querySelector('[data-js="button-darkmode"]');
+const body = document.querySelector('[data-js="body"]');
+const buttonDarkmode = document.querySelector('[data-js="button-darkmode"]');
 
-// buttonDarkmode?.addEventListener("click", () => {
-//   body.classList.toggle("darkmode");
-// });
+buttonDarkmode?.addEventListener("click", () => {
+  body.classList.toggle("darkmode");
+});
